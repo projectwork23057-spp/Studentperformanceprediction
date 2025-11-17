50b6235d7c3d4c9b04688d75f949fc55675a05e4
projectwork23057-spp <projectwork23057@gmail.com>
2025-11-17 15:48:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,16 +13,17 @@ st.title("Student Performance Prediction System")
 
 # User input
 #feature1 = st.slider("Feature 1", 0.0, 10.0, 5.0)
-Thirdsem=st.slider("scaled",300,900,1)
-Studyhour=st.slider("Studyhour",1,4,1)
-Attendance=st.slider("Attendance",60,100,1)
-Health=st.slider("health",0,1,1)
-InternetAccess=st.slider("InternetAccess",0,1,1)
-Region=st.slider("Region",0,1,1)
-DaysScholarORhosteler=st.slider("D/H",0,1,1)
-Time=st.slider("Time",5,40,1)
-ParentsEducated=st.slider("Parent's educated",0,1,1)
-ClassResponse=st.slider("class response",0,2,1)
+A=st.slider("scaled",300,900,1)
+B=st.slider("Studyhour",1,4,1)
+C=st.slider("Attendance",60,100,1)
+D=st.slider("health",0,1,1)
+E=st.slider("InternetAccess",0,1,1)
+F=st.slider("Region",0,1,1)
+G=st.slider("D/H",0,1,1)
+H=st.slider("Time",5,40,1)
+I=st.slider("Parent's educated",0,1,1)
+J=st.slider("class response",0,2,1)
+
 st.write(type((Thirdsem)))
 st.write(type((Studyhour)))
 st.write(type((Attendance)))
@@ -39,9 +40,10 @@ st.write(type((ClassResponse)))
 # Make prediction
 if st.button("Predict"):
     # Assuming your model expects a DataFrame or specific input format
-    input_data = pd.DataFrame([['Thirdsem','Studyhour','Attendance','Health','InternetAccess','Region','DaysScholarORhosteler','Time','ParentsEducated','ClassResponse']], columns=['scaled','Study hours','Attendance','health','Internet Access','Region','D/H','Time',"Parent's educated",'class response'])
+    input_data = pd.DataFrame([['A','B','C','D','E','F','G','H','I','J']], columns=['scaled','Study hours','Attendance','health','Internet Access','Region','D/H','Time',"Parent's educated",'class response'])
     prediction = model.predict(input_data)
     st.write(f"The prediction is: {prediction[0]}")
+
 
 
 
