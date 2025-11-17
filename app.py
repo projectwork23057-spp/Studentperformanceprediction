5091ffc896282268ddad88f2a1bad18c2fb0d60c
projectwork23057-spp <projectwork23057@gmail.com>
2025-11-17 15:52:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,16 +24,16 @@ H=st.slider("Time",5,40,1)
 I=st.slider("Parent's educated",0,1,1)
 J=st.slider("class response",0,2,1)
 
-st.write(type((Thirdsem)))
-st.write(type((Studyhour)))
-st.write(type((Attendance)))
-st.write(type((Health)))
-st.write(type((InternetAccess)))
-st.write(type((Region)))
-st.write(type((DaysScholarORhosteler)))
-st.write(type((Time)))
-st.write(type((ParentsEducated)))
-st.write(type((ClassResponse)))
+st.write(type((A)))
+st.write(type((B)))
+st.write(type((C)))
+st.write(type((D)))
+st.write(type((E)))
+st.write(type((F)))
+st.write(type((G)))
+st.write(type((H)))
+st.write(type((I)))
+st.write(type((J)))
 # Preprocess input (if necessary)
 # ...
 
@@ -43,6 +43,7 @@ if st.button("Predict"):
     input_data = pd.DataFrame([['A','B','C','D','E','F','G','H','I','J']], columns=['scaled','Study hours','Attendance','health','Internet Access','Region','D/H','Time',"Parent's educated",'class response'])
     prediction = model.predict(input_data)
     st.write(f"The prediction is: {prediction[0]}")
+
 
 
 
